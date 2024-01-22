96a615719fa806871a56ed3613aa80c68b93d3cb
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-01-23 00:57:58 +0330

    submit/expense and submit/income web services

--- web/views.py
+++ web/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from json import JSONEncoder
+
+from datetime import datetime
+
+from web.models import Expense, Income, Token, User
+
+
+@csrf_exempt
+def submit_expense(request):
+    """user submits an expense"""
+
+    # TODO: validate data. user might be fake. token might be fake. amount might be...
+    this_token = request.POST.get('token')
+    this_user = User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Expense.objects.create(
+        user=this_user,
+        amount=request.POST.get('amount'),
+        text=request.POST.get('text'),
+        date=date,
+    )
+
+    return JsonResponse({
+        'status': 'ok',
+    },  encoder=JSONEncoder)
+
+@csrf_exempt
+def submit_income(request):
+    """user submits an income"""
+
+    # TODO: validate data. user might be fake. token might be fake. amount might be...
+    this_token = request.POST.get('token')
+    this_user = User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Income.objects.create(
+        user=this_user,
+        amount=request.POST.get('amount'),
+        text=request.POST.get('text'),
+        date=date,
+    )
+
+    return JsonResponse({
+        'status': 'ok',
+    },  encoder=JSONEncoder)
